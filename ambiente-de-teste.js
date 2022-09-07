c7dbeee7e0186b6199edffc70617fedde954fec5
Sants16 <jvsantosrocha980@gmail.com>
2022-09-07 12:56:57 -0300

    tcc-ideia-trofeuTarefa

--- ambiente-de-teste.js
+++ ambiente-de-teste.js
@@ -1,7 +1,16 @@
-let str = 'Hello' //aqui definimos uma string a qual vamos inverter
-// fazendo so str.reverse() não é possível obter o resultado que queremos pois o js não vai entender que queremos inverter a sequencia das letras ja que ele ve toda a string como algo so
-const revStr = str.split('').reverse().join('') //seguindo esse pensamento usamos o metodo .split(), dessa forma iremos dividir todos os caracteres da nossa string por aspas simples ('') dentro de um array => ['H','e','l','l','o']
-//depois usamos o metodo .reverse() e invertemos a sequencia dos itens do array de caracter => ['o','l','l','e','H']
-//e em seguida juntamos todos os itens em uma so string => 'olleH'
+const tarefas = [
+    {
+        completed: true
+    },
+    {
+        completed: true
+    },
+    {
+        completed: false
+    }
+]
 
-console.log(revStr)
+const tarefasCompleted = tarefas.filter(tarefa => tarefa.completed)
+if(tarefasCompleted.length >= 2){
+    console.log('Parabens vc ganhou um trofeu')
+}
